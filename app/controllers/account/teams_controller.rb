29eba53816de12665d1e7937367fb4aaf5834f47
Rich Steinmetz <hey@richsteinmetz.com>
2025-05-31 17:37:25 +0200

    Add dashboard functionality

--- app/controllers/account/teams_controller.rb
+++ app/controllers/account/teams_controller.rb
@@ -1,6 +1,19 @@
 class Account::TeamsController < Account::ApplicationController
   include Account::Teams::ControllerBase
 
+  def show
+    super
+
+    dev_tips_path = Rails.root.join("public", "devmarketingtips.txt")
+    rails_reasons_path = Rails.root.join("public", "railsisdeadreasons.txt")
+
+    dev_tips = File.exist?(dev_tips_path) ? File.readlines(dev_tips_path).map(&:strip).reject(&:empty?) : []
+    rails_reasons = File.exist?(rails_reasons_path) ? File.readlines(rails_reasons_path).map(&:strip).reject(&:empty?) : []
+
+    @random_tip = dev_tips.sample if dev_tips.any?
+    @random_reason = rails_reasons.sample if rails_reasons.any?
+  end
+
   private
 
   def permitted_fields
