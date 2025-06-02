a78683841478d6275c47ac9bc0d686e24f3b4805
Rich Steinmetz <hey@richsteinmetz.com>
2025-06-02 18:19:48 +0200

    Change welcome email url to password reset url

--- app/mailers/funnel_user_mailer.rb
+++ app/mailers/funnel_user_mailer.rb
@@ -2,7 +2,7 @@ class FunnelUserMailer < ApplicationMailer
   def welcome(user, funnel_name = nil)
     @user = user
     @funnel_name = funnel_name
-    @cta_url = new_user_password_url
+    @cta_url = edit_password_url(user, reset_password_token: @user.send(:set_reset_password_token))
     @values = {
       funnel_name: @funnel_name || "our funnel",
       first_name: @user.first_name || "there"
